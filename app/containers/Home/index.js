47e5d04dd9b55e92564350cad3802cd80e3a70bf
theClubhou.se <Rebeccavl@outlook.com>
2017-05-19 13:13:30 -0400

    frontend design

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -13,11 +13,55 @@ import Footer from "components/Footer";
 
 export default class Home extends React.PureComponent {
   render() {
+  const box1={
+              height:"520px",
+              width:"auto",
+              background:"#bdbec0",
+              borderRadius:"0% 0% 0% 20%"
+              }
+  const box2={
+              height:"500px",
+              width:"auto",
+              background:"#0784f9",
+              borderRadius:"0% 0% 0% 50%"
+              }
+  const box3={
+              height:"500px",
+              width:"auto",
+              display:"flex",
+              flexDirection:"row"
+              }
+const infobox1={
+                height:"300px",
+                width:"300px",
+                background:"#000000",
+                margin:"10%"
+                }
+const infobox2={
+                height:"300px",
+                width:"300px",
+                background:"#000000",
+                margin:"10%"
+                }
+const infobox3={
+                height:"300px",
+                width:"300px",
+                background:"#000000",
+                margin:"10%"
+                }
     return (
       <div>
         <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
         <NavBar/>
-        
+        <div style={box1}>
+          <div style={box2}>
+          </div>
+        <div style={box3}>
+          <div style={infobox1}></div>
+          <div style={infobox2}></div>
+          <div style={infobox3}></div>
+        </div>
+        </div>
         <Footer/>
       </div>
     );
