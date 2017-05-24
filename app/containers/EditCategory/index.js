465ef79268a6df79bb4ee894f0a9ee454cfab2ed
theClubhou.se <Rebeccavl@outlook.com>
2017-05-24 15:48:09 -0400

    editCategory page work

--- app/containers/EditCategory/index.js
+++ app/containers/EditCategory/index.js
@@ -10,14 +10,144 @@ import Responsive from 'react-responsive';
 import {Link} from "react-router";
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 
 export default class EditCategory extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      categories: []
+    };
+  }
+  componentWillMount(){
+    fetch('http://localhost:8000/api/getCategories')
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(json){
+      this.setState({
+        categories:json
+      })
+    }.bind(this))
+  }
+
+  handleChange = (event,index,value) =>
+    this.setState({value});
+
+  handleCatergoryName = (event) => {
+    this.setState({
+      categoryName:event.target.value
+    })
+  };
+
+  handleImage = (event) => {
+    event.preventDefault();
+    let reader = new FileReader();
+    let file = event.target.files[0];
+    reader.onloadend = () => {
+      this.setState({
+        image: file,
+        preview: reader.result
+      })
+    }
+    reader.readAsDataURL(file);
+  };
+
   render() {
+
+    const mainStyle={
+                    display:"flex",
+                    flexDirection:"column",
+                    alignItems:"center"
+                    }
+    const editName={
+                    height:"200px",
+                    width:"auto",
+                    fontFamily:"PT Sans",
+                    fontFamily:"1em",
+                    color:"#000000"
+                    }
+    const editPic={
+                    height:"200px",
+                    width:"auto",
+                    fontFamily:"PT Sans",
+                    fontFamily:"1em"
+                    }
+    const editDesc={
+                    height:"200px",
+                    width:"auto",
+                    fontFamily:"PT Sans",
+                    fontFamily:"1em"
+                    }
+    const changebox={
+                    display:"flex",
+                    flexDirection:"flexDirection",
+                    alignItems:"center"
+                    }
+  const selectStyle={
+                    fontFamily:"PT Sans",
+                    fontSize:"1em",
+                    margin:"10px",
+                    height:"auto",
+                    width:"300px",
+                    background:"#ffffff",
+                    border:"2px solid",
+                    }
+    const fillStyle={
+                    fontFamily:"PT Sans",
+                    fontSize:"1em",
+                    margin:"10px",
+                    height:"auto",
+                    width:"300px",
+                    background:"#ffffff",
+                    border:"2px solid",
+                    margin:"30px"
+                    }
+    const fileStyle={
+                    fontFamily:"PT Sans",
+                    fontSize:"1em",
+                    paddingTop:"0",
+                    fontVariant:"small-caps",
+                    margin:"30px",
+                    height:"auto",
+                    width:"300px",
+                    background:"#ffffff",
+                    }
+const previewStyle={
+                width:"100px",
+                height:"100px",
+                margin:"30px"
+                }
     return (
       <div>
         <Helmet title="EditCategory" meta={[ { name: 'description', content: 'Description of EditCategory' }]}/>
-
-      //Remove this line and you can start writing your code here.
+        <NavBar/>
+        <main style={mainStyle}>
+          <div style={editName}> Edit Category Name </div>
+          <div style={changebox}>
+            <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+              {this.state.categories.map((category,index) =>(
+                <MenuItem value={category.id} primaryText={category.category} style={fillStyle}/>
+              ))}
+            </DropDownMenu>
+            <input type="text" placeholder="new category name" value={this.state.categoryName} onChange={this.handleCatergoryName} style={fillStyle}/>
+          </div>
+          <div style={editPic}> Edit Category Picture </div>
+          <div style={changebox}>
+            <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+              {this.state.categories.map((category,index) =>(
+                <MenuItem value={category.id} primaryText={category.image} style={fillStyle}/>
+              ))}
+            </DropDownMenu>
+            <input type="file" onChange={this.handleImage} style={fileStyle}/>
+            <img style={previewStyle} src={this.state.preview}/>
+            
+          </div>
+          <div style={editDesc}> Edit Category Description </div>
+        </main>
+        <Footer/>
       </div>
     );
   }
